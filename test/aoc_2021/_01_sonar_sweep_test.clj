10711c4d63e7e55f1a8e9bd3840381116bdbd25a
Kumar <aquaraga@gmail.com>
2021-12-02 13:07:59 +0530

    Move out input data into separate namespace

--- test/aoc_2021/_01_sonar_sweep_test.clj
+++ test/aoc_2021/_01_sonar_sweep_test.clj
@@ -2,8 +2,10 @@
   (:require [clojure.test :refer :all]
             [aoc-2021._01-sonar-sweep :refer :all]))
 
-(deftest a-test
+(deftest count-increases-test
   (testing "should count increases in a list"
-    (is (= 3 (count-increases [1 2 3 2 2 1 4 3]))))
+    (is (= 3 (count-increases [1 2 3 2 2 1 4 3])))))
+
+(deftest sliding-window-sums-test
   (testing "should count sums of three sliding windows"
     (is (= '(6 7 7 5 7 8) (sliding-window-sums [1 2 3 2 2 1 4 3])))))
