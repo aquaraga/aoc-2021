208d9757794b0e2c1efd3483bad3daca9be5c9e5
Kumar <aquaraga@gmail.com>
2021-12-03 16:55:28 +0530

    Be specific to binary for simplicity

--- src/aoc_2021/_03_binary_diagnostic.clj
+++ src/aoc_2021/_03_binary_diagnostic.clj
@@ -1,17 +1,16 @@
 (ns aoc-2021._03-binary-diagnostic
   (:require [clojure.string :as str] [aoc-2021.__data :refer :all]))
 
-(defn elem-by-freq [elems comparator-fn]
-  ((comp first first)(sort-by val comparator-fn (frequencies elems))))
-(defn most-common-elem [elems] (elem-by-freq elems >))
-(defn least-common-elem [elems] (elem-by-freq elems <))
+(defn count-where [elem elems] (count (filter #(= % elem) elems)))
+(def ones (partial count-where \1))
+(def zeroes (partial count-where \0))
 
 (defn slice-seq [elems]
   (letfn [(slice [elems i] (str/join "" (map #(nth % i) elems)))]
     (map #(slice elems %) (range (count (nth elems 0))))))
 
-(defn gamma [elems] (str/join "" (map most-common-elem (slice-seq elems))))
-(defn epsilon [elems] (str/join "" (map least-common-elem (slice-seq elems))))
+(defn gamma [elems] (str/join "" (map #(if (> (ones %) (zeroes %)) \1 \0) (slice-seq elems))))
+(defn epsilon [elems] (str/join "" (map #(if (< (ones %) (zeroes %)) \1 \0) (slice-seq elems))))
 
 (defn -main [& _]
   (println "Power consumption "
