674e5e48fb6e49b3e2a469113fe13d8623b19089
Kumar <aquaraga@gmail.com>
2021-12-04 15:22:10 +0530

    o2 and co2 ratings

--- src/aoc_2021/_03_binary_diagnostic.clj
+++ src/aoc_2021/_03_binary_diagnostic.clj
@@ -10,10 +10,23 @@
   (letfn [(slice [elems i] (str/join "" (map #(nth % i) elems)))]
     (map #(slice elems %) (range (count (nth elems 0))))))
 
-(defn gamma [elems] (str/join "" (map #(if (> (ones %) (zeroes %)) \1 \0) (slice-seq elems))))
+(defn gamma [elems] (str/join "" (map #(if (>= (ones %) (zeroes %)) \1 \0) (slice-seq elems))))
 (defn epsilon [elems] (str/join "" (map #(if (< (ones %) (zeroes %)) \1 \0) (slice-seq elems))))
+
+(defn gas-rating [elems fun]
+  (loop [iter 0 acc elems]
+    (let [fun-val (fun acc) filtered (filter #(= (nth fun-val iter) (nth % iter)) acc)]
+      (if (= 1 (count filtered))
+        (first filtered)
+        (recur (inc iter) filtered)))))
+
+(defn oxygen-rating [elems] (gas-rating elems gamma))
+(defn co2-rating [elems] (gas-rating elems epsilon))
 
 (defn -main [& _]
   (println "Power consumption "
            (* (Integer/parseInt (gamma report) 2)
-              (Integer/parseInt (epsilon report) 2))))
+              (Integer/parseInt (epsilon report) 2)))
+  (println "Life support rating "
+           (* (Integer/parseInt (oxygen-rating report) 2)
+              (Integer/parseInt (co2-rating report) 2))))
