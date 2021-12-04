3a74db8c4e1d3a28603360bb940dd23bd5191aec
Kumar <aquaraga@gmail.com>
2021-12-05 01:58:17 +0530

    Reformat code

--- src/aoc_2021/_04_bingo.clj
+++ src/aoc_2021/_04_bingo.clj
@@ -11,8 +11,7 @@
        (map #(to-int-vec (str/split (str/trim %) #"\s+")))
        (partition 5)
        (map #(vec (flatten %)))
-       (vec)
-       ))
+       (vec)))
 
 (defn col-partition [board]
   (map (fn [gp] (map (fn [x] (last x)) gp))
@@ -21,13 +20,11 @@
 (defn has-won [board]
   (let [winner [-1 -1 -1 -1 -1]]
     (or (some #(= winner %) (partition 5 board))
-       (some #(= winner %) (col-partition board))))
-  )
+        (some #(= winner %) (col-partition board)))))
 
 (defn play [boards input]
   (map #(let [index (.indexOf % input)]
-          (if (= -1 index) % (assoc % index -1))) boards)
-  )
+          (if (= -1 index) % (assoc % index -1))) boards))
 
 (defn score [board input]
   (* input (reduce + (filter #(not= -1 %) board))))
@@ -37,8 +34,7 @@
     (let [winning-boards (filter has-won acc) current-input (first inputs)]
       (if (seq winning-boards)
         (score (first winning-boards) last-played)
-        (recur (play acc current-input) current-input (rest inputs)))))
-  )
+        (recur (play acc current-input) current-input (rest inputs))))))
 
 (defn -main [& _]
   (println (winning-board-score (get-bingo-boards bingo-boards) bingo-inputs)))
